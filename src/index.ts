5ec21dd1d18405707bdf58129d3455561c56ecb7
Thiago <thiagovidaportela@gmail.com>
2026-01-19 17:03:14 -0300

    chore: tested caps calculation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Sistema Pip-Boy v4.2 Iniciado...")
+import { calculateCapsIncomeFromActivity } from './modules/economy/index.js'
+
+console.log("Sistema Pip-Boy v6.0 Iniciado...")
+
+console.log("Iniciando caso de teste: XP Semanal = 0 xp e XP de atividade = 100 xp")
+
+const semana1 = calculateCapsIncomeFromActivity(100, 0)
+
+console.log(semana1)
+
+console.log("Iniciando caso de teste: XP Semanal = 240 xp e XP de atividade = 20 xp")
+
+const semana2 = calculateCapsIncomeFromActivity(20, 240)
+
+console.log(semana2)
+console.log("Iniciando caso de teste: XP Semanal = 300 xp e XP de atividade = 50 xp")
+
+const semana3 = calculateCapsIncomeFromActivity(50, 300)
+
+console.log(semana3)
